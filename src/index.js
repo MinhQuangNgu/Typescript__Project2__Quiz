da65eeb07d24324867095148c75da5a4bcb0af33
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-04-01 15:46:14 +0700

    Revert "updating"
    This reverts commit fe705fd66f18996ab604ee937f9ba74ada569a8d.

--- src/index.js
+++ src/index.js
@@ -5,19 +5,20 @@ const router = require("./api/routes/index");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(express.json());
-const url = "https://quangquiz.netlify.app";
 app.use(
 	cors({
-		origin: url,
+		origin: "https://quangquiz.netlify.app/",
 	})
 );
 dotenv.config();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 const config = require("./config/database.config");
+const redis = require("./config/redis.config");
 config.mongoDB();
 config.cloudinaryConfig();
 router(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`You are connecting in port :${PORT}`);
