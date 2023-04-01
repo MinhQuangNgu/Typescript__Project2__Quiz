69d95fa567e04b932ef5c3170a2ef94dc196dd15
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-04-01 15:44:08 +0700

    updatring

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,8 +1,9 @@
 const Redis = require("redis");
+require("dotenv").config();
 let redisClient = "";
 (async () => {
 	redisClient = Redis.createClient({
-		url: "redis://default:RcScKV39SVEBbNaKfinFwEMKp9Abwi94@redis-11661.c1.asia-northeast1-1.gce.cloud.redislabs.com:11661",
+		url: process.env.REDIS_URL,
 	});
 	redisClient.on("error", (err) => console.error("Redis Client Error", err));
 	redisClient.on("ready", function () {
